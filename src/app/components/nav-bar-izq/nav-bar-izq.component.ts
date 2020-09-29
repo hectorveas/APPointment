c25224c8db749517779e45021a1d5819b76b0c61
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2020-09-29 04:13:20 -0300

    se agrego componente crisi

---
+++ src/app/components/nav-bar-izq/nav-bar-izq.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-var-izq',
+  templateUrl: './nav-bar-izq.component.html',
+  styleUrls: ['./nav-bar-izq.component.less']
+})
+export class NavBarIzqComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
