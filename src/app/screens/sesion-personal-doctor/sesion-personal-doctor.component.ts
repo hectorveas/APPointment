abf34ac576a702e1ad1bb65ab19ef5dc1eb38810
CrisiSaave <lllaf25@gmail.com>
2020-09-29 00:57:41 -0300

    se agregaron las funcionalidades de los botones de modificar de cada elemento en el fomrulario, de sesion personal doctor

--- src/app/screens/sesion-personal-doctor/sesion-personal-doctor.component.ts
+++ src/app/screens/sesion-personal-doctor/sesion-personal-doctor.component.ts
@@ -7,11 +7,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SesionPersonalDoctorComponent implements OnInit {
   public URLfoto: string;
+  public ModificarNombres: string;
+  public ModificarApellidos: string;
+  public ModificarEmail: string;
+  public ModificarFono: string;
+  public ModificarContrasena: string;
+  public ModificarRut  : string;
+
   constructor() {
       this.URLfoto='https://falabella.scene7.com/is/image/Falabella/12005138_1?wid=1500&hei=1500&qlt=70';
+      this.ModificarNombres='disabled';
+      this.ModificarApellidos='disabled';
+      this.ModificarEmail='disabled';
+      this.ModificarFono='disabled';
+      this.ModificarContrasena='disabled';
+      this.ModificarRut='disabled';
    }
 
+  modificaNombres() {
+    if(this.ModificarNombres === 'disabled'){
+      this.ModificarNombres = ''
+    } else[
+      this.ModificarNombres = 'disabled'
+    ]
+    
+  } 
+  modificaApellidos() {
+    if(this.ModificarApellidos === 'disabled'){
+      this.ModificarApellidos = ''
+    } else[
+      this.ModificarApellidos = 'disabled'
+    ]
+    
+  } 
+  modificaEmail() {
+    if(this.ModificarEmail === 'disabled'){
+      this.ModificarEmail = ''
+    } else[
+      this.ModificarEmail = 'disabled'
+    ]
+    
+  } 
+  modificaFono() {
+    if(this.ModificarFono === 'disabled'){
+      this.ModificarFono = ''
+    } else[
+      this.ModificarFono = 'disabled'
+    ]
+    
+  } 
+  modificaContrasena() {
+    if(this.ModificarContrasena === 'disabled'){
+      this.ModificarContrasena= ''
+    } else[
+      this.ModificarContrasena = 'disabled'
+    ]
+    
+  } 
+  modificaRut() {
+    if(this.ModificarRut === 'disabled'){
+      this.ModificarRut = ''
+    } else[
+      this.ModificarRut = 'disabled'
+    ]
+    
+  } 
+
   ngOnInit(): void {
+    
+    
   }
 
+
+
 }
+
+
+
