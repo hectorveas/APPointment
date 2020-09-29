08676514165e1d3fb14a63d04388b124e3eba0a0
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2020-09-29 04:46:36 -0300

    cambios redundantes 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { IncioScreenComponent } from './screens/incio-screen/incio-screen.compon
 import { InicioSesionScreenComponent } from './screens/inicio-sesion-screen/inicio-sesion-screen.component';
 import { RegistrarseScreenComponent } from './screens/registrarse-screen/registrarse-screen.component';
 import { SesionPersonalCitasDoctorComponent } from './screens/sesion-personal-citas-doctor/sesion-personal-citas-doctor.component';
+import { SesionPersonalDoctorComponent } from './screens/sesion-personal-doctor/sesion-personal-doctor.component';
 
 
 const routes: Routes = [
@@ -11,7 +12,8 @@ const routes: Routes = [
   {path: 'inicio-sesion-screen', component: InicioSesionScreenComponent },
   {path: 'inicio-screen', component: IncioScreenComponent},
   {path: 'registrarse-screen', component: RegistrarseScreenComponent },
-  {path: 'sesion-personal-citas-doctor', component: SesionPersonalCitasDoctorComponent},
+  {path: 'sesion-personal-citas-doctor', component: SesionPersonalCitasDoctorComponent },
+  {path: 'sesion-personal-doctor', component: SesionPersonalDoctorComponent},
   {path: '**', redirectTo:'/inicio-screen'}
 ];
 
