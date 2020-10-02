dcd3baaae168dda1ebebfdb038fe217acf8d7b52
Tito <hveas10@gmail.com>
2020-10-02 05:29:20 -0300

    modificancion formulario pancho

--- src/app/screens/sesion-personal-ayuda-doctor/sesion-personal-ayuda-doctor.component.ts
+++ src/app/screens/sesion-personal-ayuda-doctor/sesion-personal-ayuda-doctor.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sesion-personal-ayuda-doctor',
@@ -6,16 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sesion-personal-ayuda-doctor.component.less']
 })
 export class SesionPersonalAyudaDoctorComponent implements OnInit {
-  
-  public screenHeight: number;
-  constructor() { }
 
-  
+  public screenHeight: number;
+  checkoutForm: FormGroup;
+  mensaje: string = "";
+  isDivVisible = false;
+
+  constructor(private router: Router) {
+    this.checkoutForm = this.createFormGroup();
+  }
+
+
   ngOnInit(): void {
 
     let { height } = window.screen;
 
     this.screenHeight = height;
   }
+
+  createFormGroup() {
+    return new FormGroup(
+      {
+        usuario: new FormControl('', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$")]),
+        nombre: new FormControl('', [Validators.required]),
+        apellido: new FormControl('', [Validators.required]),
+        rut: new FormControl('', [Validators.required]),
+        consulta: new FormControl('', [Validators.required]),
+      }
+    );
+  }
+
+  onSubmit() {
+    this.mensaje = "Datos enviados";
+    this.isDivVisible = true;
+  }
+
+  get usuario() { return this.checkoutForm.get('usuario'); }
+  get nombre() { return this.checkoutForm.get('nombre'); }
+  get apellido() { return this.checkoutForm.get('apellido'); }
+  get rut() { return this.checkoutForm.get('rut'); }
+  get consulta() { return this.checkoutForm.get('consulta'); }
 
 }
